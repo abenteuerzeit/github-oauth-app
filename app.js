3014b9443a781d1dd40fb35c5febdd061b2f8914
abenteuerzeit <amroz.muzyka@gmail.com>
2023-01-28 21:24:03 +0100

    implement passport session serializers

--- app.js
+++ app.js
@@ -37,7 +37,13 @@ passport.use(new GitHubStrategy({
   }
 ));
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
 
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 
 /*
